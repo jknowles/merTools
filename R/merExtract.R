b4979b3fe78a476f6edc145392fc540f2abc9d62
Jared Knowles <jknowles@gmail.com>
2015-04-20 13:53:49 -0500

    travis and doco

--- R/merExtract.R
+++ R/merExtract.R
@@ -6,6 +6,7 @@
 #' @return a data frame with random effects from lme4
 #' @export
 REextract <- function(mod){
+  stopifnot(class(mod) == "lmerMod" | class(mod) == "glmerMod")
   out <- lme4::ranef(mod, condVar = TRUE)
   out.se <- plyr::adply(attr(out[[1]], which = "postVar"), c(3),
                         function(x) sqrt(diag(x)))
@@ -18,7 +19,7 @@ REextract <- function(mod){
 #' @title Simulate random effects from merMod
 #' @name REsim
 #' @description Simulate random effects from merMod object posterior distributions
-#' @param x a merMod object from the lme4 package
+#' @param mod a merMod object from the lme4 package
 #' @param nsims number of simulations to use
 #' @param OR logical, should parameters be converted to odds ratios?
 #' @importFrom arm sim
@@ -27,8 +28,9 @@ REextract <- function(mod){
 #' @details Use the Gelman sim technique to build empirical Bayes estimates.
 #'  Uses the sim function in the arm package
 #' @export
-REsim <- function(x, nsims, OR = FALSE){
-  mysim <- arm::sim(x, n.sims = nsims)
+REsim <- function(mod, nsims, OR = FALSE){
+  stopifnot(class(mod) == "lmerMod" | class(mod) == "glmerMod")
+  mysim <- arm::sim(mod, n.sims = nsims)
   reDims <- length(mysim@ranef)
   tmp.out <- vector("list", reDims)
   for(i in c(1:reDims)){
@@ -52,7 +54,7 @@ REsim <- function(x, nsims, OR = FALSE){
 #' @title Simulate fixed effects from merMod
 #' @name FEsim
 #' @description Simulate fixed effects from merMod object posterior distributions
-#' @param x a merMod object from the lme4 package
+#' @param mod a merMod object from the lme4 package
 #' @param nsims number of simulations to use
 #' @importFrom arm sim
 #' @import lme4
@@ -60,8 +62,9 @@ REsim <- function(x, nsims, OR = FALSE){
 #' @details Use the Gelman sim technique to build fixed effect estimates and
 #' confidence intervals. Uses the sim function in the arm package
 #' @export
-FEsim <- function(x, nsims){
-  mysim <- arm::sim(x, n.sims = nsims)
+FEsim <- function(mod, nsims){
+  stopifnot(class(mod) == "lmerMod" | class(mod) == "glmerMod")
+  mysim <- arm::sim(mod, n.sims = nsims)
   means <- apply(mysim@fixef, MARGIN = 2, mean)
   medians <- apply(mysim@fixef, MARGIN = 2, median)
   sds <- apply(mysim@fixef, MARGIN =2, sd)
@@ -70,13 +73,27 @@ FEsim <- function(x, nsims){
   return(dat)
 }
 
-# RMSE.merMod <- function(mod){
-#   # Express RMSE as percentage of dependent variable standard deviation
-#   dvSD <- sd(mod@frame[, 1])
-#   RMSE <- sqrt(mean(residuals(mod)^2))
-#   return(RMSE/dvSD)
-# }
-#
+#' @title Estimate the Root Mean Squared Error (RMSE) for a lmerMod
+#' @name RMSE.merMod
+#' @description Extract the Root Mean Squared Error for a lmerMod object
+#' @param mod a lmerMod object from the lme4 package
+#' @param scale logical, should the result be returned on the scale of
+#' response variable standard deviations?
+#' @import lme4
+#' @return a numeric which represents the RMSE
+#' @export
+RMSE.merMod <- function(mod, scale = FALSE){
+  stopifnot(class(mod) == "lmerMod")
+  # Express RMSE as percentage of dependent variable standard deviation
+  dvSD <- sd(mod@frame[, 1])
+  RMSE <- sqrt(mean(residuals(mod)^2))
+  if(scale == TRUE){
+    return(RMSE/dvSD)
+  } else{
+    return(RMSE)
+  }
+}
+
 # bootRMSE <- function(data, indx, model){
 #   moddat <- data[indx, ]
 #   modb <- eval(model@call)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # merTools
+
+[![Travis-CI Build Status](https://travis-ci.org/jknowles/merTools.png?branch=master)](https://travis-ci.org/jknowles/merTools)
+
 Convenience functions for working with merMod objects from lme4
