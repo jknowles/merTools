1159b137d6c09e00d7e0c902ead8aefb69cdf2e3
Carl Frederick <carlbfrederick@gmail.com>
2015-08-10 12:46:46 -0500

    Fixed highlight of significant RFX

--- R/merPlots.R
+++ R/merPlots.R
@@ -54,7 +54,7 @@ plotREsim <- function(data, level = 0.95, stat = "median", sd = TRUE,
 
   p <- ggplot(data, aes_string(x = "xvar", y = stat, ymax = "ymax", ymin = "ymin")) +
          geom_hline(yintercept = hlineInt, color = I("red"), size = I(1.1)) +
-         geom_point(data=subset(data, sig==FALSE), alpha=I(0.25)) +
+         geom_point(data=subset(data, sig==FALSE), color="gray75", alpha=1/(nrow(data)^.33), size=I(0.5)) +
          geom_point(data=subset(data, sig==TRUE), size=I(3)) +
          labs(x = "Group", y = "Effect Range", title = "Effect Ranges") +
          theme_bw() +
@@ -67,7 +67,9 @@ plotREsim <- function(data, level = 0.95, stat = "median", sd = TRUE,
     p <- p + scale_x_discrete(limits=c(levels(data[,"xvar"])))
   }
   if (sd) {
-    p <- p + geom_pointrange(alpha = I(0.25))
+    p <- p +
+      geom_pointrange(data=subset(data, sig==FALSE), alpha = 1/(nrow(data)^.33)) +
+      geom_pointrange(data=subset(data, sig==TRUE), alpha = 0.25)
   }
   p
 }
