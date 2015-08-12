72e876bb8a6f13992e8d7285e7a3bd22d825a60c
Jared Knowles <jknowles@gmail.com>
2015-08-11 20:40:48 -0400

    fix errors on r-devel with namespace and imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,17 @@
 # Global variables
 utils::globalVariables(c(".shinyMerPar", "sig"))
+
+# Namespace fixes
+
+#' @importFrom methods as
+#' @importFrom stats AIC as.formula formula logLik median model.matrix na.omit
+#' pnorm qnorm quantile residuals rgamma rnorm sd sima vcov weighted.mean
+zzz <- function(){
+  # Nothing
+  importFrom("methods", "as")
+  importFrom("stats", "AIC", "as.formula", "formula", "logLik", "median",
+             "model.matrix", "na.omit", "pnorm", "qnorm", "quantile",
+             "residuals", "rgamma", "rnorm", "sd", "sigma", "vcov",
+             "weighted.mean")
+
+}
