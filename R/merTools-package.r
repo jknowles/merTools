7bd95211703362bac525055ed2caca2f03101dac
Jared Knowles <jknowles@gmail.com>
2015-10-20 15:19:12 -0500

    begin adding some canonical datasets

--- R/merTools-package.r
+++ R/merTools-package.r
@@ -67,3 +67,77 @@ NULL
 #' head(hsb)
 "hsb"
 
+#' A subset of data from the 1982 High School and Beyond survey used as examples for HLM software
+#' @description A key example dataset used for examples in the HLM software manual.
+#' Included here for use in replicating HLM analyses in R.
+#' @format A data frame with 7,185 observations on the following 8 variables.
+#' \describe{
+#'  \item{\code{schid}}{a numeric vector, 160 unique values}
+#'  \item{\code{mathach}}{a numeric vector for the performance on a standardized math assessment}
+#'  \item{\code{female}}{a numeric vector coded 0 for male and 1 for female}
+#'  \item{\code{ses}}{a numeric measure of student socio-economic status}
+#'  \item{\code{minority}}{a numeric vector coded 0 for white and 1 for non-white students}
+#'  \item{\code{schtype}}{a numeric vector coded 0 for public and 1 for private schools}
+#'  \item{\code{meanses}}{a numeric, the average SES for each school in the data set}
+#'  \item{\code{size}}{a numeric for the number of students in the school}
+#' }
+#' @details The data file used for this presentation is a subsample from Chapter 10
+#' in the HLM Student Edition data files. It uses EG1HMLM2.SAV, EG2.SAV, and EG3.SAV.
+#' @source Data made available by UCLA Institute for Digital Research and Education
+#' (IDRE) online: \url{http://www.ats.ucla.edu/stat/hlm/seminars/hlm6/mlm_hlm6_seminar.htm}
+#' @references Stephen W. Raudenbush and Anthony S. Bryk (2002). Hierarchical
+#' Linear Models: Applications and Data Analysis Methods (2nd ed.). SAGE.
+#' @examples
+#' data(thai_ret)
+#' head(thai_ret)
+"thai_ret"
+
+#' A subset of data from the 1982 High School and Beyond survey used as examples for HLM software
+#' @description A key example dataset used for examples in the HLM software manual.
+#' Included here for use in replicating HLM analyses in R.
+#' @format A data frame with 7,185 observations on the following 8 variables.
+#' \describe{
+#'  \item{\code{schid}}{a numeric vector, 160 unique values}
+#'  \item{\code{mathach}}{a numeric vector for the performance on a standardized math assessment}
+#'  \item{\code{female}}{a numeric vector coded 0 for male and 1 for female}
+#'  \item{\code{ses}}{a numeric measure of student socio-economic status}
+#'  \item{\code{minority}}{a numeric vector coded 0 for white and 1 for non-white students}
+#'  \item{\code{schtype}}{a numeric vector coded 0 for public and 1 for private schools}
+#'  \item{\code{meanses}}{a numeric, the average SES for each school in the data set}
+#'  \item{\code{size}}{a numeric for the number of students in the school}
+#' }
+#' @details The data file used for this presentation is a subsample from Chapter 10
+#' in the HLM Student Edition data files. It uses NYS1.SAV, NYS2.SAV.
+#' @source Data made available by UCLA Institute for Digital Research and Education
+#' (IDRE) online: \url{http://www.ats.ucla.edu/stat/hlm/seminars/hlm6/mlm_hlm6_seminar.htm}
+#' @references Stephen W. Raudenbush and Anthony S. Bryk (2002). Hierarchical
+#' Linear Models: Applications and Data Analysis Methods (2nd ed.). SAGE.
+#' @examples
+#' data(nys)
+#' head(nys)
+"nys"
+
+#' A subset of data from the 1982 High School and Beyond survey used as examples for HLM software
+#' @description A key example dataset used for examples in the HLM software manual.
+#' Included here for use in replicating HLM analyses in R.
+#' @format A data frame with 7,185 observations on the following 8 variables.
+#' \describe{
+#'  \item{\code{schid}}{a numeric vector, 160 unique values}
+#'  \item{\code{mathach}}{a numeric vector for the performance on a standardized math assessment}
+#'  \item{\code{female}}{a numeric vector coded 0 for male and 1 for female}
+#'  \item{\code{ses}}{a numeric measure of student socio-economic status}
+#'  \item{\code{minority}}{a numeric vector coded 0 for white and 1 for non-white students}
+#'  \item{\code{schtype}}{a numeric vector coded 0 for public and 1 for private schools}
+#'  \item{\code{meanses}}{a numeric, the average SES for each school in the data set}
+#'  \item{\code{size}}{a numeric for the number of students in the school}
+#' }
+#' @details The data file used for this presentation is a subsample from Chapter 10
+#' in the HLM Student Edition data files. It uses NYS1.SAV, NYS2.SAV.
+#' @source Data made available by UCLA Institute for Digital Research and Education
+#' (IDRE) online: \url{http://www.ats.ucla.edu/stat/hlm/seminars/hlm6/mlm_hlm6_seminar.htm}
+#' @references Stephen W. Raudenbush and Anthony S. Bryk (2002). Hierarchical
+#' Linear Models: Applications and Data Analysis Methods (2nd ed.). SAGE.
+#' @examples
+#' data(mlbook)
+#' head(mlbook)
+"mlbook"
