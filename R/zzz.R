975e735b06ccc7808c4e588d8e9e1688ce4d7771
Jared Knowles <jknowles@gmail.com>
2016-02-17 15:27:10 -0600

    fix typo

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@ utils::globalVariables(c(".shinyMerPar", "sig", "sigma", "Lind"))
 #' @importFrom methods as
 #' @importFrom stats AIC as.formula formula logLik median model.matrix na.omit
 #' pnorm qnorm quantile residuals rgamma rnorm sd vcov weighted.mean delete.response
-#' model.rame na.pass reformulate runif terms
+#' model.frame na.pass reformulate runif terms
 #' @importFrom utils packageVersion
 zzz <- function(){
   # Nothing
