846e450129a0699a63cd05ccb8806b47f10eaa78
Jared Knowles <jknowles@gmail.com>
2018-06-04 12:03:56 -0400

    Travis build

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,5 @@
 library(testthat)
 library(merTools)
 
-test_check("myPkg", filter = "^[a-m]")
-test_check("myPkg", filter = "^[n-z]")
+test_check("merTools", filter = "^[a-m]")
+test_check("merTools", filter = "^[n-z]")
